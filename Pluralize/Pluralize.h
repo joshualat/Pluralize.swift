66a3c08f4e35f3529d142306443f4b7394575215
nathan spindel <nathans@gmail.com>
2015-05-28 09:38:21 -0700

    Add support for OS X, added stock .gitignore, removed joshualat.xcuserdatad

--- Pluralize/Pluralize.h
+++ Pluralize/Pluralize.h
@@ -6,7 +6,7 @@
 //  Copyright (c) 2014 Joshua Lat. All rights reserved.
 //
 
-#import <UIKit/UIKit.h>
+#import <Foundation/Foundation.h>
 
 //! Project version number for Pluralize.
 FOUNDATION_EXPORT double PluralizeVersionNumber;
